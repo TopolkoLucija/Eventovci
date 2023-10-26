dc31a24f6c147a2080da88b35e051cf9966ffe52
IvaSv <is54019@fer.hr>
2023-10-26 12:32:53 +0200

    Dodan entitet Membership(clanarina) i azurirana sql skripta

--- IzvorniKod/Backend/Spring-boot/src/main/resources/data.sql
+++ IzvorniKod/Backend/Spring-boot/src/main/resources/data.sql
@@ -1,28 +1,30 @@
 -- Create the 'korisnik' table
-CREATE TABLE korisnik (
+CREATE TABLE KORISNIK (
                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
                           korisnicko_ime VARCHAR(255) NOT NULL,
                           email VARCHAR(255) NOT NULL,
                           lozinka VARCHAR(255) NOT NULL,
                           tip_korisnika VARCHAR(13) NOT NULL,
                           adresa VARCHAR(255),
-                          placanje_clanarine BOOLEAN
+                          placanje_clanarine BOOLEAN,
+                          UNIQUE (korisnicko_ime, email)
 );
 
 -- Create the 'dogadjanje' table
-CREATE TABLE dogadjanje (
+CREATE TABLE DOGADJANJE (
                             id_dogadjanja BIGINT AUTO_INCREMENT PRIMARY KEY,
                             naziv_dogadjanja VARCHAR(255) NOT NULL,
                             tip_dogadjanja VARCHAR(255) NOT NULL,
                             lokacija_dogadjanja VARCHAR(255) NOT NULL,
-                            vrijeme_dogadjanja TIMESTAMP,
+                            vrijeme_dogadjanja TIMESTAMP NOT NULL,
                             trajanje DOUBLE PRECISION,
-                            organizator_id BIGINT,
-                            cijena_ulaznice DOUBLE PRECISION,
-                            FOREIGN KEY (organizator_id) REFERENCES korisnik(id)
+                            organizator_id BIGINT NOT NULL,
+                            cijena_ulaznice DOUBLE PRECISION NOT NULL,
+                            FOREIGN KEY (organizator_id) REFERENCES korisnik(id),
+                            UNIQUE (organizator_id)
 );
 -- Create the 'poveznica' table
-CREATE TABLE poveznica (
+CREATE TABLE POVEZNICA (
                            id_poveznice BIGINT AUTO_INCREMENT PRIMARY KEY,
                            organizator_id BIGINT,
                            link VARCHAR(255),
@@ -30,7 +32,7 @@ CREATE TABLE poveznica (
 );
 
 -- Create the 'medijski_sadrzaj' table
-CREATE TABLE medijski_sadrzaj (
+CREATE TABLE MEDIJSKI_SADRZAJ (
                                   id_medijskog_sadrzaja BIGINT AUTO_INCREMENT PRIMARY KEY,
                                   adresa_medijskog_sadrzaja VARCHAR(255),
                                   id_dogadjanja BIGINT,
@@ -38,12 +40,12 @@ CREATE TABLE medijski_sadrzaj (
 );
 
 -- Create the 'recenzija' table
-CREATE TABLE recenzija (
+CREATE TABLE RECENCIJA (
                            id_rezenczije BIGINT AUTO_INCREMENT PRIMARY KEY,
-                           recenzija_tekst TEXT,
-                           ocjena INT,
-                           id_dogadjanja BIGINT,
-                           id_korisnik BIGINT,
+                           recenzija_tekst TEXT NOT NULL ,
+                           ocjena INT NOT NULL,
+                           id_dogadjanja BIGINT NOT NULL ,
+                           id_korisnik BIGINT NOT NULL ,
                            FOREIGN KEY (id_dogadjanja) REFERENCES dogadjanje(id_dogadjanja),
                            FOREIGN KEY (id_korisnik) REFERENCES korisnik(id)
 );
@@ -55,7 +57,18 @@ CREATE TABLE dolazak_korisnika (
                                    id_dogadjanja BIGINT,
                                    id_korisnik BIGINT,
                                    FOREIGN KEY (id_dogadjanja) REFERENCES dogadjanje(id_dogadjanja),
-                                   FOREIGN KEY (id_korisnik) REFERENCES korisnik(id)
+                                   FOREIGN KEY (id_korisnik) REFERENCES korisnik(id),
+                                   UNIQUE(id_dogadjanja, id_korisnik)
 );
+
+-- Create the 'clanarina' table
+CREATE TABLE CLANARINA (
+                           id_clanarine BIGINT AUTO_INCREMENT PRIMARY KEY,
+                           id_korisnik BIGINT NOT NULL ,
+                           cijena_clanarine DOUBLE PRECISION NOT NULL ,
+                           vrijedi_do TIMESTAMP NOT NULL ,
+                           FOREIGN KEY (id_korisnik) REFERENCES korisnik(id)
+);
+
 
 
