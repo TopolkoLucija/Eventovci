a9c297f6cbf534e8995f006fa6664c502c05a60f
BrunoMarkovic <bm54215@fer.hr>
2023-10-27 15:02:12 +0200

    povezivanje logina s bazom

--- IzvorniKod/Backend/Spring-boot/src/main/java/progi/project/eventovci/EventovciApplication.java
+++ IzvorniKod/Backend/Spring-boot/src/main/java/progi/project/eventovci/EventovciApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventovciApplication {
+public class
+EventovciApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventovciApplication.class, args);
