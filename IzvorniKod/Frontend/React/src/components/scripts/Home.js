3502faa09318d3d33c3f9a9a68a347f6a9cf62ad
kvesicivaa <ik53458@fer.hr>
2023-11-04 15:39:23 +0100

    Dorađivanje početne stranice

--- IzvorniKod/Frontend/React/src/components/scripts/Home.js
+++ IzvorniKod/Frontend/React/src/components/scripts/Home.js
@@ -6,12 +6,14 @@ function Home() {
     <div className='content'>
       <div className='main-content'>
         <div className='main-content-title'>
-          <h1>Connectima</h1>
+          <h1>CONNECTIMA</h1>
+        </div>
+        <div className='main-content-image'>
+          <img src="https://media.istockphoto.com/id/1189205501/photo/cheering-crowd-of-unrecognized-people-at-a-rock-music-concert-concert-crowd-in-front-of.jpg?s=612x612&w=0&k=20&c=_vgyStdIBHCbnDHdu3lNTwfJxt2fTcJc9PB345ryhZo=" alt="Naslovna slika" />
         </div>
         <div className='main-content-text'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ea omnis inventore libero esse, unde illo,
-            tenetur aut quis facere iusto atque nobis pariatur necessitatibus vel? Harum recusandae exercitationem numquam!</p>
-          <a href='/about-us' className='more-info'>More info</a>
+          <p>Dobrodošli na mjesto povezivanja i razmjene informacija putem zabavnih događaja.</p>
+          <p>Za više informacija <a href='/about-us' className='more-info'>kliknite ovdje</a>.</p>
         </div>
       </div>
     </div>
