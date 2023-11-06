f56540082e340b8e4255d99237cb2cd2f047ffd5
TopolkoLucija <lucija.topolko@fer.hr>
2023-11-06 13:52:05 +0100

    Produženo trajanje tokena

--- IzvorniKod/Backend/Spring-boot/src/main/java/progi/project/eventovci/securityconfig/SecurityConstants.java
+++ IzvorniKod/Backend/Spring-boot/src/main/java/progi/project/eventovci/securityconfig/SecurityConstants.java
@@ -5,6 +5,6 @@ import io.jsonwebtoken.security.Keys;
 
 public class SecurityConstants {
 
-    public static final long JWT_EXPIRATION = 70000;
+    public static final long JWT_EXPIRATION = 10800000; // 2 sata
 
 }
