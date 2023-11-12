85d369713551db7fade17bb6e1ef5b0373b1a43e
TopolkoLucija <lucija.topolko@fer.hr>
2023-11-12 20:56:36 +0100

    Revert "Dodan administrator"
    This reverts commit 18dbfaf0e513cce3dff75f662d7a6f6e5ef920fb.

--- IzvorniKod/Backend/Spring-boot/src/main/java/progi/project/eventovci/securityconfig/DataLoader.java
+++ IzvorniKod/Backend/Spring-boot/src/main/java/progi/project/eventovci/securityconfig/DataLoader.java
@@ -1,8 +1,6 @@
 package progi.project.eventovci.securityconfig;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import progi.project.eventovci.user.entity.User;
 import progi.project.eventovci.user.repository.UserRepository;
@@ -12,16 +10,13 @@ public class DataLoader implements CommandLineRunner {
 
     private final UserRepository userRepository;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
     public DataLoader(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        User user = new User("admin", "admin@eventovci.com", passwordEncoder.encode("password"), "administrator", "Ilica 1", false);
+        User user = new User("admin", "admin@eventovci.com", "$2a$10$N0qnEFuZlTuUaJ82aCqC9u0gV/txgl7r4ntinYsOZIgaDIT3KwPLW", "administrator", "Ilica 1", false);
         userRepository.save(user);
     }
 }
