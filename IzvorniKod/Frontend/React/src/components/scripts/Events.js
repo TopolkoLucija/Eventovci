d4d83080c645941369c637610b4b838eeb815cf2
kvesicivaa <ik53458@fer.hr>
2023-12-04 18:29:10 +0100

    Dorada "Events.js"
    Preusmjeravanje korisnika na "PRIJAVA" ako nije prijavljen i klikne na "DOGAĐANJA" u navigacijskoj traci

--- IzvorniKod/Frontend/React/src/components/scripts/Events.js
+++ IzvorniKod/Frontend/React/src/components/scripts/Events.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Dogadaj from "./Dogadaj";
 import "../styles/Events.css";
+
 const dogadaj = {
   Naslov: "Shrek",
   Datum: "05.01.2024",
@@ -8,7 +10,46 @@ const dogadaj = {
   Poster:
     "https://m.media-amazon.com/images/M/MV5BOGZhM2FhNTItODAzNi00YjA0LWEyN2UtNjJlYWQzYzU1MDg5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
 };
+
 function Events() {
+  const accessToken = sessionStorage.getItem('accessToken');
+
+  // console.log(accessToken);
+
+  const setUserData = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (accessToken !== null) {
+      fetch('/api/data', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': accessToken
+        }
+      })
+          .then((response) => {
+            if (!response.ok) {
+              console.error('Request failed');
+              navigate('/login');
+              return;
+            }
+            // console.log(response.json());
+            return response.json();
+          })
+          .then((data) => {
+            setUserData(data);
+            // console.log(data);
+          })
+          .catch((error) => {
+            console.error('Error: ' + error);
+          });
+    }
+    else {
+      navigate('/login');
+    }
+  }, []);
+
   return (
     <div>
       {dogadaj ? (
