f1b038bb999497561aebdae6e028b9a757874812
TopolkoLucija <lucija.topolko@fer.hr>
2023-12-12 09:35:17 +0100

    Formatiranje datuma

--- IzvorniKod/Frontend/React/src/components/scripts/Dogadaj.js
+++ IzvorniKod/Frontend/React/src/components/scripts/Dogadaj.js
@@ -1,24 +1,26 @@
 import React from "react";
+import { format } from "date-fns";
 import "../styles/Dogadaj.css";
-const Dogadaj = ({ Datum, Poster, Mjesto, Naslov }) => {
-  return (
-    <div className="dogadaj">
-      <div>
-        <p>{Datum}</p>
-      </div>
-      <div>
 
-        <img
-          src={Poster !== "N/A" && Poster != null ? `data:image/png;base64,${Poster}` : "https://via.placeholder.com/400"}
-          alt={Naslov}
-        ></img>
-      </div>
-      <div>
-        <span>{Mjesto}</span>
-        <h3>{Naslov}</h3>
-      </div>
-    </div>
-  );
+const Dogadaj = ({ Datum, Poster, Mjesto, Naslov }) => {
+    const formattedDate = format(new Date(Datum), "dd. MM. yyyy  HH:mm");
+    return (
+        <div className="dogadaj">
+            <div>
+                <p>{formattedDate}</p>
+            </div>
+            <div>
+                <img
+                    src={Poster !== "N/A" && Poster != null ? `data:image/png;base64,${Poster}` : "https://via.placeholder.com/400"}
+                    alt={Naslov}
+                ></img>
+            </div>
+            <div>
+                <span>{Mjesto}</span>
+                <h3>{Naslov}</h3>
+            </div>
+        </div>
+    );
 };
 
 export default Dogadaj;
