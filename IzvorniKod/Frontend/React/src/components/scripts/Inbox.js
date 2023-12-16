ad2a14abf464548c7b18f932393904f57d3af1a8
BrunoMarkovic <bm54215@fer.hr>
2023-12-16 15:09:04 +0100

    Inbox

--- IzvorniKod/Frontend/React/src/components/scripts/Inbox.js
+++ IzvorniKod/Frontend/React/src/components/scripts/Inbox.js
@@ -1,12 +1,99 @@
-import React from 'react';
-import '../styles/Inbox.css';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Dogadaj from "./Dogadaj";
+import "../styles/Inbox.css";
 
 const Inbox = () => {
-  return (
-     <>
-        <h1>Inbox</h1>
-     </>
-  );
-}
+  const accessToken = sessionStorage.getItem("accessToken");
+  const [dogadajcic, setdogadajcic] = useState([]);
+  const setUserData = useState("");
+  const navigate = useNavigate();
 
-export default Inbox; 
+  useEffect(() => {
+    if (accessToken !== null) {
+      fetch("/api/data", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: accessToken,
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            console.error("Request failed");
+            navigate("/login");
+            return;
+          }
+          // console.log(response.json());
+          return response.json();
+        })
+        .then((data) => {
+          //setUserData(data);
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error("Error: " + error);
+        });
+    } else {
+      navigate("/login");
+    }
+  }, []);
+  const posaljiZahtjevInbox = async () => {
+    try {
+      const accessToken = sessionStorage.getItem("accessToken");
+      // const filter = x;
+
+      const response = await fetch(`/api/events/inbox`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: accessToken,
+        },
+      });
+
+      if (!response.ok) {
+        alert("Greska");
+        return;
+      }
+
+      const data = await response.json();
+
+      setdogadajcic(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  useEffect(() => {
+    posaljiZahtjevInbox();
+  }, []);
+  useEffect(() => {
+    console.log(dogadajcic);
+  }, [dogadajcic]);
+
+  return (
+    <div className="inboxContainer">
+      <h1 className="inBoxCenter">Inbox</h1>
+      <div className="KontejnerZaKarice">
+        {dogadajcic?.length > 0 ? (
+          <div className="container">
+            {dogadajcic.map((dogadaj) => {
+              return (
+                <Dogadaj
+                  key={dogadaj.id}
+                  Mjesto={dogadaj.location}
+                  Datum={dogadaj.timeOfTheEvent}
+                  Naslov={dogadaj.eventName}
+                  Poster={dogadaj.media}
+                ></Dogadaj>
+              );
+            })}
+          </div>
+        ) : (
+          <div className="empty"></div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Inbox;
