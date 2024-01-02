845b1800ff422016fa20746e337918e6c1f73016
BrunoMarkovic <bm54215@fer.hr>
2024-01-02 19:18:34 +0100

    pocetak implementacije iksica

--- IzvorniKod/Frontend/React/src/components/scripts/Dogadaj.js
+++ IzvorniKod/Frontend/React/src/components/scripts/Dogadaj.js
@@ -44,7 +44,29 @@ const Dogadaj = ({ Id, Datum, Poster, Mjesto, Naslov, display }) => {
       setPrikazi(false);
     }
   }, [vrstaPosjetitelja]);
-
+  const izbrisiDogadaj = async (event) => {
+    event.preventDefault();
+    console.log("Stisnija si");
+    alert("eto");
+    // try {
+    //   const accessToken = sessionStorage.getItem("accessToken");
+    //   const response = await fetch(`/api/events/all/${filter}`, {
+    //     method: "GET",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Authorization: accessToken,
+    //     },
+    //   });
+    //   if (!response.ok) {
+    //     console.log("Nije oke response, mozda nisi prijavljen");
+    //     return;
+    //   }
+    //   const data = await response.json();
+    //   setdogadajcic(data);
+    // } catch (error) {
+    //   console.error("Error:", error);
+    // }
+  };
   return (
     <a href={`/event/${Id}`}>
       <div className="dogadaj">
@@ -58,6 +80,7 @@ const Dogadaj = ({ Id, Datum, Poster, Mjesto, Naslov, display }) => {
               fill="currentColor"
               className="bi bi-x"
               viewBox="0 0 16 16"
+              onClick={izbrisiDogadaj}
             >
               <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
             </svg>
