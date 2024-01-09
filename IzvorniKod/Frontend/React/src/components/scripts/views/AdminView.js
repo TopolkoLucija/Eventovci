9bcf318565c8fd1b377c552d975fc9cc10e72181
filipmiskovic <fm134913696@fer.hr>
2024-01-09 13:31:01 +0100

    Poboljšanje
    - poravnati su korisnici na popisu korisnika

--- IzvorniKod/Frontend/React/src/components/scripts/views/AdminView.js
+++ IzvorniKod/Frontend/React/src/components/scripts/views/AdminView.js
@@ -18,6 +18,7 @@ const AdminView = (props) => {
       content: ""
    });
    const [amount, setAmount] = useState("");
+   const [filteredUsers, setFilteredUsers] = useState([{}]);
 
    useEffect(() => {
       setEmail(userData.email);
@@ -250,54 +251,106 @@ const AdminView = (props) => {
          })
          .then((response) => {
             if (response) {
-               if (document.querySelector('.user')) {
-                  document.querySelector('.allUsers').innerHTML = "";
-               }
-               response.forEach((res) => {
+               setFilteredUsers(response);
 
-                  if (res.typeOfUser === "administrator") {
-                     return;
+               // ako je širina ekrana veća od 768px!!!
+               if (window.innerWidth > 768) {
+                  if (document.querySelector('.tableRowBody')) {
+                     document.querySelector('.tbody').innerHTML = "";
+                  }
+                  
+                  response.forEach((res) => {
+                     if (res.typeOfUser === "administrator") {
+                        return;
+                     }
+                     
+                     var tableBody = document.querySelector('.tbody');
+                     var tableRow = document.createElement('tr');
+                     tableRow.className = "tableRowBody";
+                     tableRow.id = `${res.userId}`;
+                     var tdUserName = document.createElement('td');
+                     tdUserName.innerText = res.username;
+                     // dodavanje event listenera za navigaciju na detalje organizatora
+                     tdUserName.addEventListener("click", () => {
+                        if (res.typeOfUser === "organizator") {
+                           navigate(`/organizer/${res.userId}`);
+                        }
+                     });
+                     var tdEmail = document.createElement('td');
+                     tdEmail.innerText = res.email;
+                     var tdTypeOfUser = document.createElement('td');
+                     tdTypeOfUser.innerText = `(${res.typeOfUser})`;
+                     var tdButtonDelete = document.createElement('td');
+                     const buttonDeleteTable = document.createElement('button');
+                     buttonDeleteTable.innerHTML = "&times";
+                     buttonDeleteTable.classList = "btn btn-primary";
+                     buttonDeleteTable.classList = "delete-button";
+                     buttonDeleteTable.id = `${res.userId}`;
+                     buttonDeleteTable.addEventListener("click", handleDeleteUser);
+
+                     tdButtonDelete.append(buttonDeleteTable);
+
+                     tableRow.append(tdUserName);
+                     tableRow.append(tdEmail);
+                     tableRow.append(tdTypeOfUser);
+                     tableRow.append(tdButtonDelete);
+
+                     tableBody.append(tableRow);
+
+                  })
+               }
+               else {
+                  if (document.querySelector('.user')) {
+                     document.querySelector('.allUsers').innerHTML = "";
                   }
 
-                  var divAllUsers = document.querySelector('.allUsers');
-                  const divUser = document.createElement('div');
-                  const spanUserName = document.createElement('span');
-                  const spanEmail = document.createElement('span');
-                  const spanTypeOfUser = document.createElement('span');
-                  const buttonDelete = document.createElement('button');
+                  response.forEach((res) => {
+                     var divAllUsers = document.querySelector('.allUsers');
+                     const divUser = document.createElement('div');
+                     const divUserData = document.createElement('div');
+                     const divButtonDelete = document.createElement('div');
+                     const spanUserName = document.createElement('span');
+                     const spanEmail = document.createElement('span');
+                     const spanTypeOfUser = document.createElement('span');
+                     const buttonDelete = document.createElement('button');
 
-                  divUser.append(spanUserName);
-                  divUser.append(spanEmail);
-                  divUser.append(spanTypeOfUser);
-                  divUser.append(buttonDelete);
+                     divUserData.append(spanUserName);
+                     divUserData.append(spanEmail);
+                     divUserData.append(spanTypeOfUser);
+                     divButtonDelete.append(buttonDelete);
 
-                  divAllUsers.append(divUser);
+                     divUser.append(divButtonDelete);
+                     divUser.append(divUserData);
 
-                  spanUserName.innerText = res.username;
-                  spanEmail.innerText = res.email;
-                  spanTypeOfUser.innerText = `(${res.typeOfUser})`;
-                  buttonDelete.innerHTML = "&times";
+                     divAllUsers.append(divUser);
 
-                  spanUserName.id = "username-span";
-                  spanEmail.id = "email-span";
-                  spanTypeOfUser.id = "typeOfUser-span";
+                     spanUserName.innerText = res.username;
+                     spanEmail.innerText = res.email;
+                     spanTypeOfUser.innerText = `(${res.typeOfUser})`;
+                     buttonDelete.innerHTML = "&times";
 
-                  divUser.className = "user";
-                  divUser.id = `${res.userId}`;
-                  buttonDelete.classList = "btn btn-primary";
-                  buttonDelete.classList = "delete-button";
-                  buttonDelete.id = `${res.userId}`;
+                     spanUserName.id = "username-span";
+                     spanEmail.id = "email-span";
+                     spanTypeOfUser.id = "typeOfUser-span";
 
-                  // dodavanje event listenera za navigaciju na detalje organizatora
-                  spanUserName.addEventListener("click", () => {
-                     if(res.typeOfUser === "organizator") {
-                     navigate(`/organizer/${res.userId}`);
-                     }
+                     divUser.className = "user";
+                     divUser.id = `${res.userId}`;
+                     divUserData.className = "div-userData";
+                     divButtonDelete.className = "div-buttonDelete";
+                     buttonDelete.classList = "btn btn-primary";
+                     buttonDelete.classList = "delete-button";
+                     buttonDelete.id = `${res.userId}`;
+
+                     // dodavanje event listenera za navigaciju na detalje organizatora
+                     spanUserName.addEventListener("click", () => {
+                        if (res.typeOfUser === "organizator") {
+                           navigate(`/organizer/${res.userId}`);
+                        }
+                     });
+
+                     buttonDelete.addEventListener("click", handleDeleteUser);
                   });
-
-                  buttonDelete.addEventListener("click", handleDeleteUser);
-
-               })
+               }
             }
          })
    }
@@ -306,111 +359,126 @@ const AdminView = (props) => {
       <>
          {
             (accessToken !== null) ? // ako je netko prijavljen onda vrati info o korisniku/organizatoru, inače ništa
-            <div className="content">
-               <div className="my-account-content">
-                  <div className='my-account-content-title-and-text'>
-                     <div className='my-account-content-title'>
-                        <h1>Pozdrav, {userData.username}!</h1>
-                        <h4>{userData.typeOfUser}</h4>
-                     </div>
-                     <div className='my-account-content-text'>
-                        <div className='form'>
-                           <div className="form-group">
-                              <label htmlFor="userName">Korisničko ime:</label>
-                              <input type="text" className="form-control" id="userName" value={username} onChange={(e) => { setUserName(e.target.value) }} disabled></input>
-                           </div>
+               <div className="content">
+                  <div className="my-account-content">
+                     <div className='my-account-content-title-and-text'>
+                        <div className='my-account-content-title'>
+                           <h1>Pozdrav, {userData.username}!</h1>
+                           <h4>{userData.typeOfUser}</h4>
+                        </div>
+                        <div className='my-account-content-text'>
+                           <div className='form'>
+                              <div className="form-group">
+                                 <label htmlFor="userName">Korisničko ime:</label>
+                                 <input type="text" className="form-control" id="userName" value={username} onChange={(e) => { setUserName(e.target.value) }} disabled></input>
+                              </div>
 
-                           <div className="form-group">
-                              <label htmlFor="email">E-mail adresa:</label>
-                              <input type="email" className="form-control" id="email" value={email} onChange={(e) => { setEmail(e.target.value) }} disabled></input>
-                           </div>
+                              <div className="form-group">
+                                 <label htmlFor="email">E-mail adresa:</label>
+                                 <input type="email" className="form-control" id="email" value={email} onChange={(e) => { setEmail(e.target.value) }} disabled></input>
+                              </div>
 
-                           <button className="btn btn-primary" onClick={validation} hidden>Spremi</button>
+                              <button className="btn btn-primary" onClick={validation} hidden>Spremi</button>
+                           </div>
                         </div>
                      </div>
+
+                     <div className="edit-content">
+                        <button className="btn btn-primary" id="edit-buttons" onClick={Edit}>Uredi profil</button>
+                        <button className="btn btn-primary" id="edit-buttons" onClick={() => {
+                           handleAllUsers(0);
+                        }}>Pogledaj sve korisnike</button>
+                        <button className="btn btn-primary" id='edit-buttons' onClick={() => {
+                           openModalIncreaseMembership();
+                        }}>Promijeni cijenu članarine</button>
+                     </div>
+
+                     {/* Modal */}
+                     {showModalValidation && (
+                        <div className="background">
+                           <div className="window">
+                              <div>Unesi lozinku</div>
+                              <label htmlFor='password'>Lozinka:</label>
+                              <input type='password' className='form-control' id='password' onChange={(e) => { setPassword(e.target.value); }}></input>
+                              <button type="submit" className='btn btn-primary' onClick={handleEdit}>Provjeri</button>
+                           </div>
+                        </div>
+                     )}
+
+                     {/* Modal */}
+                     {showModalShowAll && (
+                        <div className="background">
+                           <div className="window-show-all">
+                              <span className='exit' onClick={closeModalShowAll}>&times;</span>
+                              <div>Prikaži sve</div>
+                              <div className='option-buttons'>
+                                 <button className='btn btn-primary' onClick={() => {
+                                    handleAllUsers(0);
+                                 }}>Svi</button>
+                                 <button className='btn btn-primary' onClick={() => {
+                                    handleAllUsers(1);
+                                 }}>Posjetitelji</button>
+                                 <button className='btn btn-primary' onClick={() => {
+                                    handleAllUsers(2);
+                                 }}>Organizatori</button>
+                              </div>
+                              <div className='allUsers'>
+                              </div>
+
+                              <div className="container mt-5">
+                                 <table className="table">
+                                    <thead>
+                                       <tr>
+                                          <th scope="col">Username</th>
+                                          <th scope="col">Email</th>
+                                          <th scope="col">Type of user</th>
+                                          <th scope="col"></th>
+                                       </tr>
+                                    </thead>
+                                    <tbody className='tbody'></tbody>
+                              </table>
+                              </div>
+                           </div>
+                        </div>
+                     )}
+
+                     {/* Modal */}
+                     {showModalIncreaseMembership && (
+                        <div className="background">
+                           <div className="window">
+                              <span className='exit' onClick={closeModalIncreaseMembership}>&times;</span>
+                              <div>Unesi iznos:</div>
+                              <form>
+                                 <input type='text' className='form-control' id='amount' onChange={(e) => {
+                                    const inputValue = e.target.value;
+                                    if (/^\d+$/.test(inputValue) || inputValue === '') {
+                                       setAmount(inputValue);
+                                    }
+                                 }}
+                                 />
+                                 <button type="submit" className='btn btn-primary' onClick={handleIncreaseMembership}>Promijeni</button>
+                              </form>
+                           </div>
+                        </div>
+                     )}
+
+                     {/* Modal */}
+                     {showModalMessage && (
+                        <div className="background">
+                           <div className="window">
+                              <div>{message.content}</div>
+                              <button className='btn btn-primary' onClick={() => {
+                                 closeModalMessage();
+
+                                 if (message.type === "delete") {
+                                    sessionStorage.removeItem("accessToken");
+                                    navigate('/home');
+                                 }
+                              }}>Zatvori</button>
+                           </div>
+                        </div>
+                     )}
                   </div>
-
-                  <div className="edit-content">
-                     <button className="btn btn-primary" id="edit-buttons" onClick={Edit}>Uredi profil</button>
-                     <button className="btn btn-primary" id="edit-buttons" onClick={() => {
-                        handleAllUsers(0);
-                     }}>Pogledaj sve korisnike</button>
-                     <button className="btn btn-primary" id='edit-buttons' onClick={() => {
-                        openModalIncreaseMembership();
-                     }}>Promijeni cijenu članarine</button>
-                  </div>
-
-                  {/* Modal */}
-                  {showModalValidation && (
-                     <div className="background">
-                        <div className="window">
-                           <div>Unesi lozinku</div>
-                           <label htmlFor='password'>Lozinka:</label>
-                           <input type='password' className='form-control' id='password' onChange={(e) => { setPassword(e.target.value); }}></input>
-                           <button type="submit" className='btn btn-primary' onClick={handleEdit}>Provjeri</button>
-                        </div>
-                     </div>
-                  )}
-
-                  {/* Modal */}
-                  {showModalShowAll && (
-                     <div className="background">
-                        <div className="window-show-all">
-                           <span className='exit' onClick={closeModalShowAll}>&times;</span>
-                           <div>Prikaži sve</div>
-                           <div className='option-buttons'>
-                              <button className='btn btn-primary' onClick={() => {
-                                 handleAllUsers(0);
-                              }}>Svi</button>
-                              <button className='btn btn-primary' onClick={() => {
-                                 handleAllUsers(1);
-                              }}>Posjetitelji</button>
-                              <button className='btn btn-primary' onClick={() => {
-                                 handleAllUsers(2);
-                              }}>Organizatori</button>
-                           </div>
-                           <div className='allUsers'></div>
-                        </div>
-                     </div>
-                  )}
-
-                  {/* Modal */}
-                  {showModalIncreaseMembership && (
-                     <div className="background">
-                        <div className="window">
-                           <span className='exit' onClick={closeModalIncreaseMembership}>&times;</span>
-                           <div>Unesi iznos:</div>
-                           <form>
-                              <input type='text' className='form-control' id='amount' onChange={(e) => {
-                                     const inputValue = e.target.value;
-                                     if (/^\d+$/.test(inputValue) || inputValue === '') {
-                                        setAmount(inputValue);
-                                     }
-                                  }}
-                              />
-                              <button type="submit" className='btn btn-primary' onClick={handleIncreaseMembership}>Promijeni</button>
-                           </form>
-                        </div>
-                     </div>
-                  )}
-
-                  {/* Modal */}
-                  {showModalMessage && (
-                     <div className="background">
-                        <div className="window">
-                           <div>{message.content}</div>
-                           <button className='btn btn-primary' onClick={() => {
-                              closeModalMessage();
-
-                              if (message.type === "delete") {
-                                 sessionStorage.removeItem("accessToken");
-                                 navigate('/home');
-                              }
-                           }}>Zatvori</button>
-                        </div>
-                     </div>
-                  )}
-               </div>
                </div> : ""
          }
       </>
